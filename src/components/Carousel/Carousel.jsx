aaad5ed2d3908d0b08fe1c820f7609571e791185
Cris8049 <cristopher070404@gmail.com>
2024-06-19 12:20:53 -0400

    Banners changed

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -9,11 +9,11 @@ const banners = [
         alt: 'Banner 1'
     },
     {
-        image: 'https://cdn.shopify.com/s/files/1/0588/1380/8845/files/Virtuality_1717x592_1.png',
+        image: 'https://scontent.fsrz1-1.fna.fbcdn.net/v/t39.30808-6/400528137_736863921820244_8304446350767620788_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=lSwuNA4fSmMQ7kNvgEjE-8E&_nc_ht=scontent.fsrz1-1.fna&oh=00_AYC0_k_xuHvbGnr2ME3B2F7RASRf6Chq8wr-pbtV7ZKrmQ&oe=6675CB11',
         alt: 'Banner 2'
     },
     {
-        image: 'https://cdn.shopify.com/s/files/1/0588/1380/8845/files/Virtuality_1717x592_1.png',
+        image: 'https://scontent.fsrz1-1.fna.fbcdn.net/v/t39.30808-6/438081650_936929954795764_6452349645420143575_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=J8tv491OwLYQ7kNvgHFJL1l&_nc_ht=scontent.fsrz1-1.fna&oh=00_AYAF8aTxjYN8Nu5IxyTq8rV-4Qb6z6VaS6hyh2OfMOYE2g&oe=6675BA39',
         alt: 'Banner 3'
     }
 ];
